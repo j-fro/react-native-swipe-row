1c2e9c57a59d9bf22bf6d102b199ec0a07ed70cd
Jacob Froman <jacob.h.froman@gmail.com>
2018-01-19 12:11:29 -0600

    Update type defs

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,21 @@
 import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
 
-export default class SwipeRow extends React.Component<any> {}
+export interface SwipeRowProps extends ViewProperties {
+    rowStyle?: StyleProp<ViewStyle>;
+    leftUnderlayStyle?: StyleProp<ViewStyle>;
+    rightUnderlayStyle?: StyleProp<ViewStyle>;
+    leftUnderlay?: JSX.Element;
+    rightUnderlay?: JSX.Element;
+    canSwipeLeft?: boolean;
+    canSwipeRight?: boolean;
+    thresholdLeft?: number;
+    thresholdRight?: number;
+    thresholdLeftPercent?: number;
+    thresholdRightPercent?: number;
+    swipeSensivity?: number;
+    onSwipeLeft?(): void;
+    onSwipeRight?(): void;
+}
+
+export default class SwipeRow extends React.Component<SwipeRowProps> {}
